8a070629ec84d2e6a6422cd337a0534d5c156eb7
Anton Morgunov <a.mor@bc-pf.org>
2021-11-14 15:23:19 -0500

    feat: added protocol page

---
+++ pages/reports.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+// import { push } from "@socialgouv/matomo-next";
+import Container from "../components/container"
+import Title from "../components/title";
+import Heading from "../components/heading";
+import Paragraph from "../components/paragraph";
+import Click from "../components/click";
+import List from "../components/list";
+import Olist from "../components/orderlist";
+import SubHead from "../components/subheading";
+import Wrapper from "../components/wrapper"
+import JoinComposer from '../blocks/joincomposer';
+import JoinWatcher from '../blocks/joinwatch';
+import JoinJury from '../blocks/joinjury';
+
+const PUBPROTOCOLS = [
+    {
+        title: "Протокол №01",
+        date: "14 ноября 2021",
+        href: "/docs/public-protocols/protocol1.pdf"
+    }
+]
+export default function Reports() {
+      return (
+          <div>
+            <Head>
+                <title>Отчетность Коллегии | QazChO</title>
+                <meta name="description" content="Отчеты республиканской коллегии составителей задач по олимпиадной химии" />
+                <meta name="keywords" content="составители олимпиад в Республике Казахстан, олимпийское движение Республики Казахстан по химии, составители и жюри республиканских олимпиад" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Wrapper>
+                <Container>
+                    <Heading>Протокола собраний Коллегии:</Heading>
+                    <List>
+                        {PUBPROTOCOLS ? PUBPROTOCOLS.map((doc, i) => (
+                            <li key={i}><Click title={doc.title} href={doc.href} /> (от {doc.date})</li>
+                        )) : null}
+                    </List>
+                </Container>
+                <footer className="flex flex-row justify-center my-3">
+                    <p className="text-xs">Эта страница была обновлена 14 ноября 2021 г.</p>
+                </footer>
+            </Wrapper>
+          </div>
+      )
+}
