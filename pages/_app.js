9038acbcb8e82cd60bd78b9d25dfa6b466b7b82e
Anton Morgunov <a.mor@bc-pf.org>
2021-11-15 10:25:03 -0500

    fix: typos & errors

--- pages/_app.js
+++ pages/_app.js
@@ -16,9 +16,9 @@ function MyApp({ Component, pageProps }) {
       <nav className="sticky top-0 bg-white dark:bg-black pt-2 pb-6 opacity-90 z-10">
         <ul className="flex flex-wrap flex-row justify-center space-x-4 sm:space-x-10 mt-8">
           <li className={router.asPath == "/" ? "text-accent-500" : ""}><Link href="/">Главная</Link></li>
-          <li className={router.asPath == "/members" ? "text-accent-500" : ""}><Link href="/members">Состав</Link></li>
-          <li className={router.asPath == "/join" ? "text-accent-500" : ""}><Link href="/join">Вступить</Link></li>
-          <li className={router.asPath == "/reports" ? "text-accent-500" : ""}><Link href="/reports">Отчеты</Link></li>
+          <li className={router.asPath == "/members/" ? "text-accent-500" : ""}><Link href="/members">Состав</Link></li>
+          <li className={router.asPath == "/join/" ? "text-accent-500" : ""}><Link href="/join">Вступить</Link></li>
+          <li className={router.asPath == "/reports/" ? "text-accent-500" : ""}><Link href="/reports">Отчеты</Link></li>
         </ul>
       </nav>
       <Component {...pageProps} />
