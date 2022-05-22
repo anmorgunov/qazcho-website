27c9259ec12240146e2763c73a40171f0cab0450
Anton Morgunov <morgunov@mit.edu>
2022-05-21 22:40:46 -0400

    feat: junior final

--- pages/problems.js
+++ pages/problems.js
@@ -7,60 +7,47 @@ import List from "../components/list";
 import Wrapper from "../components/wrapper"
 import Subheading from "../components/subheading"
 
+const RESPA = [
+    {
+        title: "РХО. Задания и решения (9-11 кл.)",
+        href: "https://olympiads.bc-pf.org/chemistry/national/2022"
+    },
+]
+
 const JUNIOR = [
     {
-        title: "Областной этап. 7 кл. Задания (рус)",
-        href: "/docs/problems/junior/oblast/chem7-junior-tasks-rus.pdf"
+        title: "Финал. 7 класс. Задания (рус)",
+        href: "/docs/problems/junior/chem7-tasks.pdf"
     },
     {
-        title: "Областной этап. 7 кл. Задания (каз)",
-        href: "/docs/problems/junior/oblast/chem7-junior-tasks-qaz.pdf"
+        title: "Финал. 7 класс. Задания (каз)",
+        href: "/docs/problems/junior/chem7-tasks-qaz.pdf"
     },
     {
-        title: "Областной этап. 7 кл. Решения",
-        href: "/docs/problems/junior/oblast/chem7-junior-sol.pdf"
+        title: "Финал. 7 класс. Решения",
+        href: "/docs/problems/junior/chem7-sol.pdf"
     },
     {
-        title: "Областной этап. 8 кл. Задания (рус)",
-        href: "/docs/problems/junior/oblast/chem8-junior-tasks-rus.pdf"
+        title: "Финал. 8 класс. Задания (рус)",
+        href: "/docs/problems/junior/chem8-tasks.pdf"
     },
     {
-        title: "Областной этап. 8 кл. Задания (каз)",
-        href: "/docs/problems/junior/oblast/chem8-junior-tasks-qaz.pdf"
+        title: "Финал. 8 класс. Задания (каз)",
+        href: "/docs/problems/junior/chem8-tasks-qaz.pdf"
     },
     {
-        title: "Областной этап. 8 кл. Решения",
-        href: "/docs/problems/junior/oblast/chem8-junior-sol.pdf"
+        title: "Финал. 8 класс. Решения",
+        href: "/docs/problems/junior/chem8-sol.pdf"
     },
     {
-        title: "Районный этап. 7 кл. Задания (рус)",
-        href: "/docs/problems/junior/region/chem7-junior-tasks-rus.pdf"
-    },
-    {
-        title: "Районный этап. 7 кл. Задания (каз)",
-        href: "/docs/problems/junior/region/chem7-junior-tasks-qaz.pdf"
-    },
-    {
-        title: "Районный этап. 7 кл. Решения",
-        href: "/docs/problems/junior/region/chem7-junior-sol.pdf"
-    },
-    {
-        title: "Районный этап. 8 кл. Задания (рус)",
-        href: "/docs/problems/junior/region/chem8-junior-tasks-rus.pdf"
-    },
-    {
-        title: "Районный этап. 8 кл. Задания (каз)",
-        href: "/docs/problems/junior/region/chem8-junior-tasks-qaz.pdf"
-    },
-    {
-        title: "Районный этап. 8 кл. Решения",
-        href: "/docs/problems/junior/region/chem8-junior-sol.pdf"
+        title: "Районный и областной этапы, задания и решения (7-8 кл.)",
+        href: "https://olympiads.bc-pf.org/chemistry/s/junior/2022"
     }
 ]
 
 const OBLAST = [
     {
-        title: "ОблХО. Задачи и решения (9-11 кл.)",
+        title: "ОблХО. Задания и решения (9-11 кл.)",
         href: "https://olympiads.bc-pf.org/chemistry/oblast/2022"
     },
     {
@@ -71,7 +58,7 @@ const OBLAST = [
 
 const PROBLEMS = [
     {
-        title: "РайХО. Задачи и решения (9-11 кл.)",
+        title: "РайХО. Задания и решения (9-11 кл.)",
         href: "https://olympiads.bc-pf.org/chemistry/region/2022"
     }
 ]
@@ -93,6 +80,12 @@ export default function Reports() {
                             <li key={i}><Click title={doc.title} href={doc.href} /></li>
                         )) : null}
                     </List>
+                    <Subheading>Республиканская олимпиада</Subheading>
+                    <List>
+                        {RESPA ? RESPA.map((doc, i) => (
+                            <li key={i}><Click title={doc.title} href={doc.href} /></li>
+                        )) : null}
+                    </List>
                     <Subheading>Областной этап</Subheading>
                     <List>
                         {OBLAST ? OBLAST.map((doc, i) => (
@@ -107,7 +100,7 @@ export default function Reports() {
                     </List>
                 </Container>
                 <footer className="flex flex-row justify-center my-3">
-                    <p className="text-xs">Эта страница была обновлена 15 апреля 2022 г.</p>
+                    <p className="text-xs">Эта страница была обновлена 21 мая 2022 г.</p>
                 </footer>
             </Wrapper>
           </div>
